7d2042bcd9eefb95b0c8d1dde15d8fb3d4aafd1f
khanh Tuong <146071055+KhanhTuong3002@users.noreply.github.com>
2024-05-11 15:59:29 +0700

    add theme

---
+++ WebClient/src/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: ["**/*.cshtml"],
+    theme: {
+        extend: {},
+    },
+    daisyui: {
+        themes: [
+            "retro",
+        ],
+    },
+    plugins: [
+        require('daisyui'),
+    ],
+}

bb8210a1e76711a352c3560274517d85a2c21b25
khanh Tuong <146071055+KhanhTuong3002@users.noreply.github.com>
2024-05-17 15:24:18 +0700

    Change razopage web

--- GeoClinet/Program.cs
+++ GeoClinet/Program.cs
@@ -1,6 +1,8 @@
 using DataAccess;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +16,42 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<GeoTycoonDbcontext>();
 builder.Services.AddControllersWithViews();
 
+string storagePath = Path.Combine(Directory.GetCurrentDirectory(), "Storage", "Images");
+if (!Directory.Exists(storagePath))
+{
+    Directory.CreateDirectory(storagePath);
+}
+PhysicalFileProvider fileProvider = new(storagePath);
+builder.Services.AddSingleton(fileProvider);
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 8;
+});
+
+// Authorize role
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Teacher", policy =>
+    {
+        policy.RequireClaim(ClaimTypes.Role, "Teacher");
+    });
+    options.AddPolicy("Student", policy =>
+    {
+        policy.RequireClaim(ClaimTypes.Role, "Student", "Pending");
+    });
+    options.AddPolicy("Admin", policy =>
+    {
+        policy.RequireClaim(ClaimTypes.Role, "Administrator");
+    });
+});
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -23,7 +60,7 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -35,6 +72,38 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
+//this is test code for send mail .....
+Console.WriteLine("Sending test email");
+//await RegisterModel.SendEmailAsync("Tuongvkce161108@fpt.edu.vn", "test", "test");
 
+//default admin account
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+    var configuration = app.Configuration;
+    string email = configuration["Credentials:Email"];
+    string password = configuration["Credentials:Password"];
+
+    if (await userManager.FindByEmailAsync(email) == null)
+    {
+        var user = new IdentityUser
+        {
+            UserName = email,
+            Email = email,
+            EmailConfirmed = true,
+            NormalizedEmail = email.ToUpper(),
+            NormalizedUserName = email.ToUpper(),
+        };
+        var result = await userManager.CreateAsync(user, password);
+        if (result.Succeeded)
+        {
+            await userManager.AddToRoleAsync(user, "Administrator");
+        }
+    }
+}
 app.Run();
